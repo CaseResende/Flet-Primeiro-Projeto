2b9e191135f7c0e5a09d8629790d1ce97dd02e81
Carlos André Resende Belo <carlos_andre_resende@yahoo.com.br>
2025-04-09 17:23:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,86 @@ import flet as ft
 
 
 def main(pagina: ft.Page):
-    texto = ft.Text('Olá, Mundo!')
+    pagina.bgcolor = ft.colors.BLACK
 
-    pagina.add(texto)
+
+    def change_main_image(e):
+        for elemento in options.controls:
+            if elemento == e.control:
+                elemento.opacity = 1
+                main_image.src = elemento.content.src
+            else:
+                elemento.opacity = 0.5
+
+        main_image.update()
+        options.update()
+
+
+    imagens_produto = ft.Container(
+        bgcolor=ft.colors.WHITE,
+        padding=ft.padding.all(30),
+        aspect_ratio=9/16,
+        content=ft.Column(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            controls=[
+                main_image := ft.Image(
+                    src='https://dasports.com.br/cdn/shop/files/los-angeles-lakers-nike-icon-edition-swingman-jersey-gold-luka-don_C4_8Di_C4_87-youth_ss5_p-202849486_pv-1_u-ckv5fga0j3hsrgotxrrp_v-xalafditxpnrpdb7y4ef.png?v=1738806116',
+                ),
+
+                options := ft.Row(
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    controls=[
+                        ft.Container(
+                            content=ft.Image(
+                                src='https://dasports.com.br/cdn/shop/files/los-angeles-lakers-nike-icon-edition-swingman-jersey-gold-luka-don_C4_8Di_C4_87-youth_ss5_p-202849486_pv-1_u-ckv5fga0j3hsrgotxrrp_v-xalafditxpnrpdb7y4ef.png?v=1738806116',
+                            ),
+                            width=80,
+                            height=80,
+                            opacity=1,
+                            on_click=change_main_image
+                        ),
+                        ft.Container(
+                            content=ft.Image(
+                                src='https://images.footballfanatics.com/los-angeles-lakers/los-angeles-lakers-nike-association-edition-swingman-jersey-white-luka-don%C4%8Di%C4%87-unisex_ss5_p-202849487+pv-1+u-sa2i3cumkfim08urwvvc+v-mqltjo7yy92yntc9d7b9.jpg?_hv=2&w=900',
+                            ),
+                            width=80,
+                            height=80,
+                            opacity=0.5,
+                            on_click=change_main_image
+                        ),
+                        ft.Container(
+                            content=ft.Image(
+                                src='https://acdn-us.mitiendanube.com/stores/002/794/749/products/big-6236de797ebdf79ed417388408266384-1024-1024.jpg',
+                            ),
+                            width=80,
+                            height=80,
+                            opacity=0.5,
+                            on_click=change_main_image
+                        )
+                    ]
+                )
+            ]
+        )
+    )
+    detalhes_produto = ft.Container()
+
+    layout = ft.Container(
+        width=900,
+        margin=ft.margin.all(30),
+        shadow=ft.BoxShadow(blur_radius=300, color=ft.colors.CYAN),
+        content=ft.ResponsiveRow(
+            columns=12,
+            spacing=0,
+            run_spacing=0,
+            controls=[
+                imagens_produto,
+                detalhes_produto
+            ]
+        )
+    )
+
+
+    pagina.add(layout)
 
 ft.app(target=main)
 
